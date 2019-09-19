234e71702ee2af31121c4d3893240f2ee17fa667
happyhe <root@ops-13f734f06.qiyi.virtual>
2019-09-19 15:45:11 +0800

    init

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#########################################################################
+# File Name: build.sh
+# Author: happyhe
+# mail: heguang@qiyi.com
+# Created Time: Thu 19 Sep 2019 03:38:40 PM CST
+#########################################################################
+
+cd cpp && mkdir -p cmake && cd cmake && cmake .. && make && cd ../..
+cd swig && swig -go -cgo -intgosize 64 -c++ ./go_test_cpp.swigcxx && mkdir -p cmake && cd cmake && cmake .. && make && cd ../..
+cd go_src && go run main.go
+
